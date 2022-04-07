bd568f750f99fb610dce2df485615c8931b6b1be
Wagner.Xu <wagnerhsu@hotmail.com>
2022-04-07 11:24:46 +0800

    Add comment

--- 01/S102/Program.cs
+++ 01/S102/Program.cs
@@ -2,4 +2,5 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 app.Run(handler: handler);
+// 最后调用WebApplication另一个无参Run扩展方法是为了启动承载的应用
 app.Run();
