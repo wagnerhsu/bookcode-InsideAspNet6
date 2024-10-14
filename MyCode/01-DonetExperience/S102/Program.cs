28c8149d6c202291870caa50c32eb83e66db045f
Wagner Xu <wagnerhsu@hotmail.com>
2024-10-14 21:55:52 +0800

    feat: add files

---
+++ MyCode/01-DonetExperience/S102/Program.cs
@@ -0,0 +1,6 @@
+﻿RequestDelegate handler = context => context.Response.WriteAsync("Hello, World!");
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+WebApplication app = builder.Build();
+app.Run(handler: handler);
+// 最后调用WebApplication另一个无参Run扩展方法是为了启动承载的应用
+app.Run();
